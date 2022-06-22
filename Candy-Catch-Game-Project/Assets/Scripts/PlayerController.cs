fa88a1902891d9fb50b4b5a989058fca973657a8
myxsan <yksl3461@gmail.com>
2022-06-23 02:28:40 +0300

    Set up player movement and x position boundaries

---
+++ Candy-Catch-Game-Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool canMove = true;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float maxPos = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(canMove)
+        {
+            Move();
+        }
+        
+    }
+
+    private void Move()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+
+
+        transform.Translate(inputX * speed * Time.deltaTime, 0f, 0f);
+        float xPos = Mathf.Clamp(transform.position.x, -maxPos, maxPos);
+        transform.position = new Vector3(xPos, transform.position.y, transform.position.z);
+    }
+}
