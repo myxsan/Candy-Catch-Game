8d0f524ba98e83881d5d1b2a54f802b472f60fb7
myxsan <yksl3461@gmail.com>
2022-06-23 02:58:56 +0300

    Created candy destroyer

---
+++ Candy-Catch-Game-Project/Assets/Scripts/CandyScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+
+        if(other.tag == "Player")
+        {
+            Destroy(gameObject);
+            Debug.Log($"{this.name} Eaten");
+        }
+
+        else if(other.tag == "Boundary")
+        {
+            Destroy(gameObject);
+            Debug.Log("Destroyed");
+        }
+    }
+}
